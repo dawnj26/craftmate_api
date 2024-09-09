a6e7c7d7dcf39214bbb8a9026791e788a03ba2bd
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-09 19:59:57 +0800

    fix: hide unnecessary columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -33,6 +34,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
+        'email_verified_at',
+        'google_id',
+        'facebook_id',
     ];
 
     /**
@@ -46,5 +52,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function likes() : HasMany
+    {
+        return $this->hasMany(Like::class);
     }
 }
