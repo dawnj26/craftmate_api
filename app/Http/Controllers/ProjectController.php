9337a4bd4cf1467fb4dcdc968bbb1eb509995268
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-09 19:58:40 +0800

    feat: create and update project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use App\Models\Tag;
 use Illuminate\Http\Request;
@@ -12,8 +13,8 @@ class ProjectController extends Controller
     public function createProject(Request $request) {
         $validate = Validator::make($request->all(), [
             'title' => 'required',
-            'description' => 'nullable|string',
             'tags' => 'nullable|string',
+            'is_public' => 'required|boolean',
         ]);
 
         $user = auth()->user();
@@ -38,16 +39,10 @@ class ProjectController extends Controller
             ], 422);
         }
 
-        $description = $request->has('description') ? $request->input('description') : null;
-
-        if ($description != null) {
-            $description = json_decode($description, true);
-        }
-
         $project = Project::create([
             'user_id' => $user->id,
             'title' => $request->input('title'),
-            'description' => $description,
+            'is_public' => $request->input('is_public'),
         ]);
 
         if ($request->has('tags'))
@@ -60,11 +55,89 @@ class ProjectController extends Controller
             $project->tags()->sync($tags->pluck('id'));
         }
 
+        $project->with(['steps' => function ($query) {
+            $query->whereNull('parent_id');
+        }]);
+
         return response()->json([
             'metadata' => [
                 'status' => 201,
                 'message' => 'Project created successfully',
             ],
+            'data' => new ProjectResource($project),
         ],201);
+    }
+
+    public function getProject(Project $project)
+    {
+        return response()->json([
+            "metadata" => [
+                'status' => 200,
+                'message' => 'Project retrieved successfully',
+            ],
+            'data' => new ProjectResource($project),
+        ]);
+    }
+
+    public function updateDescription(Request $request,Project $project)
+    {
+        $validate = Validator::make($request->all(), [
+            'description' => 'required|json',
+        ]);
+
+        $user = auth()->user();
+        $projectUserId = $project->user_id;
+
+        if ($validate->fails() || $user->id != $projectUserId)
+        {
+            return response()->json([
+                'metadata' => [
+                    'status' => 422,
+                    'message' => 'Missing required fields or validation error',
+                ],
+            ], 422);
+        }
+
+        $project->description = $request->input('description');
+        $project->save();
+
+        return response()->json([
+            'metadata' => [
+                'status' => 200,
+                'message' => 'Updated successfully',
+            ],
+        ], 200);
+    }
+
+    public function updateSteps(Request $request, Project $project)
+    {
+        $validate = Validator::make($request->all(), [
+            'steps' => 'required|json',
+        ]);
+
+        $user = auth()->user();
+        $projectUserId = $project->user_id;
+
+        if ($validate->fails() || $user->id != $projectUserId)
+        {
+            return response()->json([
+                'metadata' => [
+                    'status' => 422,
+                    'message' => 'Missing required fields or validation error',
+                ],
+            ], 422);
+        }
+
+        $step = $project->step;
+
+        $step->content = $request->input('steps');
+        $step->save();
+
+        return response()->json([
+            'metadata' => [
+                'status' => 200,
+                'message' => 'Updated successfully',
+            ],
+        ], 200);
     }
 }
