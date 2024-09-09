9fa58f31c42d05604c7a635b5cf718a2b9a15ea1
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-09 19:58:52 +0800

    add: project and step resources

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $user = auth('sanctum')->user();
+
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'parent_id' => $this->parent_id,
+            'is_public' => $this->is_public,
+            'user' => $this->user,
+            'tags' => $this->tags,
+            'is_liked' => $user ? $this->isLikedByUser($user) : false,
+            'like_count' => $this->likes()->count(),
+            'comment_count' => $this->comments()->count(),
+            'fork_count' => $this->child()->count(),
+            'steps' => $this->step,
+        ];
+    }
+}
