1fc058b56d7b44e471fc79427ebd269cdc41a2da
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-17 13:41:25 +0800

    add: like count and check if the current user liked the comment

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,11 +14,15 @@ class CommentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $user = auth('sanctum')->user();
+
         return [
             'id' => $this->id,
             'content' => $this->content,
+            'like_count' => $this->likes()->count(),
             'children' => CommentResource::collection($this->whenLoaded('children')),
             'user' => new UserResource($this->whenLoaded('user')),
+            'is_liked' => $user === null ? false : $this->isLikedByUser($user),
         ];
     }
 }
