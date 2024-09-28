c5b20661315f36fca6987447811469290716c356
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-09-28 15:47:11 +0800

    add: paginated projects resource

---
+++ app/Http/Resources/ProjectCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProjectCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'items' => $this->collection,
+            'links' => [
+                'next' => $this->nextPageUrl(),
+            ],
+            'meta' => [
+                'current_page' => $this->currentPage(),
+                'per_page' => $this->perPage(),
+                'total' => $this->total(),
+                'last_page' => $this->lastPage(),
+            ],
+        ];
+    }
+}
