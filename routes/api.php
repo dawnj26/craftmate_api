3deca72f99907071ad6cf836d37d64bb835256ff
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-12 14:41:10 +0800

    add: get projects with filtration

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,16 @@ use App\Http\Controllers\ProjectsController;
 require __DIR__.'/user_auth/auth.php';
 require __DIR__.'/project/project.php';
 
-Route::get('projects', [ProjectsController::class, 'get']);
+Route::get('projects/latest', [ProjectsController::class, 'getLatest']);
+
+Route::get('user/{user}/projects', [ProjectsController::class, 'getUserProjects'])
+        ->where('user', '[0-9]+')
+        ->middleware('auth:sanctum');
+
+
+Route::get('user/projects/{visibility?}', [ProjectsController::class, 'getCurrentUserProjects'])
+        ->whereIn('visibility', [1, 2, 3])
+        ->middleware('auth:sanctum');
+
+Route::delete('user/projects/delete', [ProjectsController::class, 'deleteUserProjects'])
+        ->middleware('auth:sanctum');
