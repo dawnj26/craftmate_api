c752fc5efefde585ee063c47f3bff731f94deda8
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-10-24 10:45:45 +0800

    add: routes for material CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MaterialController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectsController;
+use App\Models\Material;
 
 require __DIR__.'/user_auth/auth.php';
 require __DIR__.'/project/project.php';
@@ -19,3 +21,16 @@ Route::get('user/projects/{visibility?}', [ProjectsController::class, 'getCurren
 
 Route::delete('user/projects/delete', [ProjectsController::class, 'deleteUserProjects'])
         ->middleware('auth:sanctum');
+
+Route::get('material-categories', [MaterialController::class, 'getMaterialCategory']);
+Route::post('material/create', [MaterialController::class, 'createMaterial'])->middleware('auth:sanctum');
+Route::get('materials/user', [MaterialController::class, 'getMaterials'])->middleware('auth:sanctum');
+Route::get('material/{material}', [MaterialController::class, 'getMaterial'])->where('material', '[0-9]+');
+Route::put('material/{material}/edit', [MaterialController::class, 'editMaterial'])->where('material', '[0-9]+')->middleware('auth:sanctum');
+Route::delete('material/{material}/delete', [MaterialController::class, 'deleteMaterial'])->where('material', '[0-9]+')->middleware('auth:sanctum');
+Route::delete('materials/delete', [MaterialController::class, 'deleteMaterials'])->middleware('auth:sanctum');
+Route::get('materials/search', [MaterialController::class, 'searchMaterials'])->middleware('auth:sanctum');
+
+Route::get('materials/project/{project}', [MaterialController::class, 'getProjectMaterials'])->where('project', '[0-9]+');
+Route::post('project/{project}/materials/add', [MaterialController::class, 'addMaterialsToProject'])->where('project', '[0-9]+')->middleware('auth:sanctum');
+Route::delete('project/{project}/materials/delete', [MaterialController::class, 'deleteProjectMaterials'])->where('project', '[0-9]+')->middleware('auth:sanctum');
