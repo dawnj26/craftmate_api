7bca594a14695c69c89f235e12b1008fb71058d0
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-05 17:47:41 +0800

    add: trending, following projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -14,6 +14,87 @@ use App\Models\Material;
 
 class ProjectsController extends Controller
 {
+    public function getTrending(Request $request)
+    {
+        $query = Project::query();
+
+        if ($request->has('timeframe')) {
+            $timeframe = $request->input('timeframe');
+            switch ($timeframe) {
+                case 'today':
+                    $query->whereDate('created_at', today());
+                    break;
+                case 'this_week':
+                    $query->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()]);
+                    break;
+                case 'this_month':
+                    $query->whereMonth('created_at', now()->month);
+                    break;
+                case 'this_year':
+                    $query->whereYear('created_at', now()->year);
+                    break;
+                case 'all_time':
+                default:
+                    // No additional constraints
+                    break;
+            }
+        }
+
+        if ($request->has('sort_by')) {
+            $sortBy = $request->input('sort_by');
+            $allowedSorts = ['views_count', 'likes_count', 'comments_count'];
+
+            if (!in_array($sortBy, $allowedSorts)) {
+                return ResponseHelper::errInput();
+            }
+
+            $order = $request->input('order', 'desc');
+            if (!in_array($order, ['asc', 'desc'])) {
+                return ResponseHelper::errInput();
+            }
+
+            if ($sortBy === 'views_count') {
+                $query->withCount('views')
+                    ->orderBy('views_count', $order);
+            } else if ($sortBy === 'likes_count') {
+                $query->withCount('likes')
+                    ->orderBy('likes_count', $order);
+            } else if ($sortBy === 'comments_count') {
+                $query->withCount('comments')
+                    ->orderBy('comments_count', $order);
+            }
+        } else {
+            $query->latest('created_at');
+        }
+
+        if ($request->has('category_id')) {
+            $categoryId = $request->input('category_id');
+            $query->where('project_category_id', $categoryId);
+        }
+
+        $projects = $query->paginate(10);
+
+        return ResponseHelper::jsonWithData(200, 'Trending projects retrieved', new ProjectCollection($projects));
+    }
+
+    public function getFollowing(Request $request)
+    {
+        $user = auth()->user();
+        $followingIds = $user->following()->pluck('id');
+        $categoryId = $request->input('category_id');
+
+        $query = Project::whereIn('user_id', $followingIds)
+            ->where('visibility_id', 1);
+
+        if ($categoryId) {
+            $query->where('project_category_id', $categoryId);
+        }
+
+        $projects = $query->latest()->paginate(10);
+
+        return ResponseHelper::jsonWithData(200, 'Projects retrieved', new ProjectCollection($projects));
+    }
+
     public function deleteUserProjects(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -46,11 +127,18 @@ class ProjectsController extends Controller
         return ResponseHelper::json(200, 'Projects deleted');
     }
 
-    public function getLatest()
+    public function getLatest(Request $request)
     {
-        $projects = Project::where('visibility_id', 1)->latest()->paginate(10);
+        $query = Project::where('visibility_id', 1)->latest();
 
-        return ResponseHelper::jsonWithData(200, 'Projects retrieved',  new ProjectCollection($projects));
+        if ($request->has('category_id')) {
+            $categoryId = $request->input('category_id');
+            $query->where('project_category_id', $categoryId);
+        }
+
+        $projects = $query->paginate(10);
+
+        return ResponseHelper::jsonWithData(200, 'Projects retrieved', new ProjectCollection($projects));
     }
 
     public function getUserProjects(Request $request, User $user)
@@ -78,6 +166,11 @@ class ProjectsController extends Controller
             $query->where('visibility_id', $visibility);
         }
 
+        if ($request->has('category_id')) {
+            $categoryId = $request->input('category_id');
+            $query->where('project_category_id', $categoryId);
+        }
+
         if ($request->has('sort_by')) {
             $sortBy = $request->input('sort_by');
             $allowedSorts = ['updated_at', 'created_at', 'like_count', 'comment_count', 'title'];
@@ -98,7 +191,6 @@ class ProjectsController extends Controller
                 $query->withCount('comments')
                     ->orderBy('comments_count', $order);
             } else {
-
                 $query->orderBy($sortBy, $order);
             }
         } else {
