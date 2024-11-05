b2f253edb798ec037211407c83d2d2fa9b63797a
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-05 17:51:50 +0800

    call the seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        $this->call([
+            MaterialCategorySeeder::class,
+            ProjectCategorySeeder::class,
+            VisibilitySeeder::class,
+        ]);
     }
 }
