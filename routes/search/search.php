07724460b28d3807ba9a6b6d7d4792780436bae9
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-05 17:52:55 +0800

    add: search route

---
+++ routes/search/search.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\SearchController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(SearchController::class)->group(function () {
+    Route::get('search/suggest', 'searchSuggest');
+    Route::get('search/projects', 'searchProjects');
+});
