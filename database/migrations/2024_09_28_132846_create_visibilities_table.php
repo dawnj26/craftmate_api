4d6c9e324fdbc00397c11c8549533cb723474eda
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-11-05 17:51:07 +0800

    correct spelling

--- database/migrations/2024_09_28_132846_create_visibilities_table.php
+++ database/migrations/2024_09_28_132846_create_visibilities_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('visibility');
+        Schema::dropIfExists('visibilities');
     }
 };
